b9bfd257a534da7eb2f8b3d600ec938032317e74
咕谷酱 <74496778+GooGuJiang@users.noreply.github.com>
2026-02-20 20:06:25 +0800

    添加部分资源

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <h1 className="text-4xl font-bold">Hello, CalabiYau Piano!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
